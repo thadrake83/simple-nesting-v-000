1da3e3d67c245731b3f16a59c2fabaf540914edb
Drake Rentrop <drakerentrop@gmail.com>
2018-11-27 19:51:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -42,6 +42,7 @@ def alan_kay_is_known_for
 		 programmer_hash[:alan_kay][:known_for]
 end
 alan_kay_is_known_for
+
 def dennis_ritchies_language
 	programmer_hash =
  		{
@@ -58,7 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
+dennis_ritchies_language
 
 def adding_matz
 # add the following information to the top level of programmer_hash
